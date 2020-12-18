1e69b52e6a834a62e2d0552924f7c196c30099ee
Apollonia Sorella <a.sorella@interface3.be>
2020-12-18 13:34:04 +0100

    changement de place du render

--- script.js
+++ script.js
@@ -28,59 +28,59 @@ function render() {
   }
 
   app.innerHTML += '<footer><div id="cart"></div><button id="booking">Passer la commande</button><button id="emptyCart">Annuler la réservation</button></footer>';
-}
+  // cacher et montrer les vaccins non-approuvés
+  const notApproved = document.querySelectorAll('.not-approved');
 
-render();
-// cacher et montrer les vaccins non-approuvés
-const notApproved = document.querySelectorAll('.not-approved');
-
-document.body.addEventListener('click', (e) => {
-  if (e.target.className === 'hide') {
-    for (const el of notApproved) {
-      el.parentNode.style.display = 'none';
+  document.body.addEventListener('click', (e) => {
+    if (e.target.className === 'hide') {
+      for (const el of notApproved) {
+        el.parentNode.style.display = 'none';
+      }
+      e.target.innerText = 'Montrer les vaccins non-approuvés';
+      e.target.classList.remove('hide');
+      e.target.classList.add('show');
+    } else if (e.target.className === 'show') {
+      render();
     }
-    e.target.innerText = 'Montrer les vaccins non-approuvés';
-    e.target.classList.remove('hide');
-    e.target.classList.add('show');
-  } else if (e.target.className === 'show') {
-    render();
-  }
-});
+  });
 
-// réservation -- affichage footer
-const reserve = document.querySelectorAll('.reserve');
-const cart = document.querySelector('#cart');
-for (const el of reserve) {
-  el.addEventListener('click', (e) => {
-    const getParent = e.target.parentNode;
-    const vaccineName = getParent.querySelector('.name').innerText;
-    const quantity = getParent.querySelector('input').value;
-    cart.innerHTML += `<br/>- ${vaccineName} x ${quantity}`;
-    getParent.querySelector('input').disabled = true;
-    el.disabled = true;
+  // réservation -- affichage footer
+  const reserve = document.querySelectorAll('.reserve');
+  const cart = document.querySelector('#cart');
+  for (const el of reserve) {
+    el.addEventListener('click', (e) => {
+      const getParent = e.target.parentNode;
+      const vaccineName = getParent.querySelector('.name').innerText;
+      const quantity = getParent.querySelector('input').value;
+      cart.innerHTML += `<br/>- ${vaccineName} x ${quantity}`;
+      getParent.querySelector('input').disabled = true;
+      el.disabled = true;
+    });
+  }
+
+  // passer la commande + bonus : impossible de passer commande si panier vide
+  const booking = document.querySelector('#booking');
+  booking.addEventListener('click', () => {
+    if (cart.innerHTML === '' || cart.innerHTML === 'Vous devez au moins sélectionner un vaccin pour commander.') {
+      cart.innerHTML = 'Vous devez au moins sélectionner un vaccin pour commander.';
+    } else {
+      app.innerHTML = `La commande a bien été enregistrée !
+  <button id="cancel">Annuler la commande</button>`;
+    }
+  });
+
+  // bonus : renvoi à la page d'origine avec un bouton annuler
+  document.body.addEventListener('click', (e) => {
+    if (e.target.matches('#cancel')) {
+      render();
+    }
+  });
+
+  // bonus: annuler la réservation
+  const emptyCart = document.querySelector('#emptyCart');
+  emptyCart.addEventListener('click', () => {
+    render();
   });
 }
 
-// passer la commande + bonus : impossible de passer commande si panier vide
-const booking = document.querySelector('#booking');
-booking.addEventListener('click', () => {
-  if (cart.innerHTML === '' || cart.innerHTML === 'Vous devez au moins sélectionner un vaccin pour commander.') {
-    cart.innerHTML = 'Vous devez au moins sélectionner un vaccin pour commander.';
-  } else {
-    app.innerHTML = `La commande a bien été enregistrée !
-  <button id="cancel">Annuler la commande</button>`;
-  }
-});
-
-// bonus : renvoi à la page d'origine avec un bouton annuler
-document.body.addEventListener('click', (e) => {
-  if (e.target.matches('#cancel')) {
-    render();
-  }
-});
-
-// bonus: annuler la réservation
-const emptyCart = document.querySelector('#emptyCart');
-emptyCart.addEventListener('click', () => {
-  render();
-});
+render();
