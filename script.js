59eb55f8e1aaf61a45363d766f34994ec6d3f90b
Apollonia Sorella <a.sorella@interface3.be>
2020-12-18 14:24:11 +0100

    check de la quantité choisie

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ import { vaccins } from './src/data';
 
 const app = document.querySelector('#app');
 
-// construction du squelette
 function render() {
   app.innerHTML = '';
   app.innerHTML += `<h1>Commande de vaccins Covid</h1>
@@ -20,14 +19,15 @@ function render() {
         Inventeurs: ${vaccins[i].inventeurs}<br/>
         Lieu de production: ${vaccins[i].lieudeproduction}<br/>
         Quantité disponible: ${vaccins[i].quantite}<br/>
-        Prix unitaire: ${vaccins[i].prixunitaire}$<br/>
+        Prix unitaire: <span class="price">${vaccins[i].prixunitaire}</span>$<br/>
         Officiellement approuvé: ${vaccins[i].approuve ? '<span class="approved">oui</span>' : '<span class="not-approved">non</span>'}<br/>
         Nombre de vaccins désiré:<br/><input type="number"> - <button class="reserve">Réserver</button>
     </div>
   `;
   }
 
-  app.innerHTML += '<footer><div id="cart"></div><button id="booking">Passer la commande</button><button id="emptyCart">Annuler la réservation</button></footer>';
+  app.innerHTML += '<footer><div id="cart"></div>Total de la commande: <div id="total"></div><button id="booking">Passer la commande</button><button id="emptyCart">Annuler la réservation</button></footer>';
+
   // cacher et montrer les vaccins non-approuvés
   const notApproved = document.querySelectorAll('.not-approved');
 
@@ -44,17 +44,21 @@ function render() {
     }
   });
 
-  // réservation -- affichage footer
+  // réservation -- affichage footer + bonus: calcul individuel du prix des doses
   const reserve = document.querySelectorAll('.reserve');
   const cart = document.querySelector('#cart');
+  // const total = document.querySelector('#total');
   for (const el of reserve) {
     el.addEventListener('click', (e) => {
       const getParent = e.target.parentNode;
       const vaccineName = getParent.querySelector('.name').innerText;
+      const vaccinePrice = getParent.querySelector('.price').innerText;
       const quantity = getParent.querySelector('input').value;
-      cart.innerHTML += `<br/>- ${vaccineName} x ${quantity}`;
-      getParent.querySelector('input').disabled = true;
-      el.disabled = true;
+      if (quantity > 0) {
+        cart.innerHTML += `<br/>- ${vaccineName} x ${quantity} = <span class="subTotal">${quantity * vaccinePrice}</span>$<br/>`;
+        getParent.querySelector('input').disabled = true;
+        el.disabled = true;
+      }
     });
   }
 
