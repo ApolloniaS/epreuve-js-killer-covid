a892230f37828797806f95d79dc17d1ddda0dcd1
Apollonia Sorella <a.sorella@interface3.be>
2020-12-18 09:36:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+import './styles.scss';
+import { vaccins } from './src/data';
+
+const app = document.getElementById('#app');
+app.innerHTML += '<h1>Commande de vaccins Covid</h1>';
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const vaccins = [
+  {
+    nom: 'BBIBP-CorV',
+    inventeurs: ['Sinopharm'],
+    lieudeproduction: ['Chine'],
+    technologie: 'virus inactivé',
+    quantite: 420,
+    prixunitaire: 17,
+    approuve: true,
+  },
+
+  {
+    nom: 'Sputnik V',
+    inventeurs: ['Gamaleya'],
+    lieudeproduction: ['Russie', 'Inde'],
+    technologie: 'adenovirus',
+    quantite: 130,
+    prixunitaire: 12,
+    approuve: false,
+  },
+  {
+    nom: 'AZD1222',
+    inventeurs: ['AstraZeneca', 'University of Oxford'],
+    lieudeproduction: ['Royaume Unis', 'Brésil', 'Inde'],
+    technologie: 'adenovirus',
+    quantite: 5028,
+    prixunitaire: 1.78,
+    approuve: false,
+  },
+
+  {
+    nom: 'Tozinameran',
+    inventeurs: ['BioNTech', 'Pfizer', 'Fosun Pharma'],
+    lieudeproduction: ['Allemagne', 'USA'],
+    technologie: 'ARN messager',
+    quantite: 980,
+    prixunitaire: 14,
+    approuve: true,
+  },
+
+  {
+    nom: 'mRNA-1273',
+    inventeurs: ['Moderna', 'NIAID', 'BARDA'],
+    lieudeproduction: 'USA',
+    technologie: 'ARN messager',
+    quantite: 240,
+    prixunitaire: 28,
+    approuve: false,
+  },
+  {
+    nom: 'Ad26.COV2.S',
+    inventeurs: ['Johnson & Johnson', 'BIDMC'],
+    lieudeproduction: ['USA', 'Amérique du Sud', 'Ukraine', 'Afrique du Sud'],
+    technologie: 'adenovirus',
+    quantite: 110,
+    prixunitaire: 31,
+    approuve: false,
+  },
+
+];
